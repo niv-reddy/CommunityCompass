05a4b27918fa902ec3b6431a1048d74f13f141c3
C0ngH0 <minhho2601@gmail.com>
2024-05-02 20:20:34 -0700

    fixing font size for header

---
+++ js/calendar.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var calendarEl = document.getElementById("calendar");
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: "dayGridMonth",
+    events: function (fetchInfo, successCallback, failureCallback) {
+      fetch("http://localhost:3000/events")
+        .then((response) => response.json())
+        .then((events) => successCallback(events))
+        .catch((error) => failureCallback(error));
+    },
+    eventContent: function (arg) {
+      return {
+        html: `<div style='color:white;'><b>${arg.event.title}</b></div>`,
+      };
+    },
+  });
+  calendar.render();
+
+  // Setup event filtering based on checkboxes
+  document
+    .querySelectorAll('input[type="checkbox"]')
+    .forEach(function (checkbox) {
+      checkbox.addEventListener("change", function () {
+        let checkedColors = Array.from(
+          document.querySelectorAll('input[type="checkbox"]:checked')
+        ).map((el) => el.value);
+        let events = calendar.getEvents();
+        events.forEach((event) => {
+          // Display or hide events based on checkbox colors
+          event.setProp(
+            "display",
+            checkedColors.includes(event.backgroundColor) ? "block" : "none"
+          );
+        });
+      });
+    });
+});
